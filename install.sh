2cc220de862d386552669773771222ce3ddbda57
waveshare <36142105+waveshare@users.noreply.github.com>
2019-10-15 13:34:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ echo $password | sudo -S apt install -y python3-pil python3-smbus
 echo $password | sudo -S python3 setup.py install
 
 # install picard display service
-sed -i -e 's:#dtparam=i2c_arm=on:dtparam=i2c_arm=on:g'  /boot/config.txt || true
+echo $password | sudo -S sed -i -e 's:#dtparam=i2c_arm=on:dtparam=i2c_arm=on:g'  /boot/config.txt || true
 python3 -m pidisplay.create_display_service
 echo $password | sudo -S mv picard_display.service /etc/systemd/system/picard_display.service
 echo $password | sudo -S systemctl enable picard_display
